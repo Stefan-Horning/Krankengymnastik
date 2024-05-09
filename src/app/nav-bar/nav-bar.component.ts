7008b1a601077b8a4b364b75763d58993332109e
Stefan-Horning <stefanhorning02704@gmail.com>
2024-05-09 21:14:32 +0200

    Finish Nav-Bar

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,14 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [CommonModule,RouterLink],
+  imports: [CommonModule,RouterLink,RouterLinkActive],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
 export class NavBarComponent {
+  isMenuOpen:boolean = false;
 
+  switch() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  stop(event: Event) {
+    event.stopPropagation();
+  }
 }
